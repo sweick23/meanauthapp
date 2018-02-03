385008896b25cd96a292d0c0604c29ea8465bd4c
Shane Weickum <sweick23@outlook.com>
2018-02-02 16:37:18 -0800

    MEAN Auth App

---
+++ app.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const config = require('./config/database');
+
+// connect to database
+mongoose.connect(config.database);
+// On Connection
+mongoose.connection.on('connected', () => {
+    console.log("Connected to Database " + config.database);
+});
+
+// On Error
+mongoose.connection.on('error', (err) => {
+    console.log('Database Error' + err);
+});
+
+const app = express();
+// Route for Users
+const users = require('./routes/users');
+
+
+// Port number
+const port = process.env.PORT || 8080;
+// CORS Middleware
+app.use(cors());
+
+//set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport')(passport);
+
+app.use('/users', users);
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'public/index.html'));
+})
+
+// Index Route
+app.get('/', (req, res) => {
+    res.send('Invalid Endpoint :)');
+})
+
+// Start Server
+app.listen(port, () => {
+    console.log('Server Started on Port ' + port);
+})
